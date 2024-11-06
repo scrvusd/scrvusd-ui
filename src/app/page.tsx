8294d9632ed403c66033bd90ac219df174ee4370
scrvusd <scrvusd@gmail.com>
2024-11-06 21:10:13 +0100

    remove spaces

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@ import { ManageStaking } from "@/components/ManageStaking";
 import { Stats } from "@/components/Stats";
 
 export default function Home() {
-
-
   return (
     <div className="flex flex-row justify-center items-stretch min-h-full gap-2 sm:p-10 p-10 font-[family-name:var(--font-geist-sans)]">
     
